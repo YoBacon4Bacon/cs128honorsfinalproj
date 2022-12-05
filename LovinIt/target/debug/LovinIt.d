85ccd98c19c33aefed1cf4c1ceb63b24268a8e26
Ryan To <rto4@illinois.edu>
2022-12-05 16:10:37 -0600

    menu

--- LovinIt/target/debug/LovinIt.d
+++ LovinIt/target/debug/LovinIt.d
@@ -1,1 +1,1 @@
-/Users/pjakka/Documents/cs128honorsfinalproj/LovinIt/target/debug/LovinIt: /Users/pjakka/Documents/cs128honorsfinalproj/LovinIt/src/main.rs
+C:\Users\ryank\cs128h\cs128honorsfinalproj\LovinIt\target\debug\LovinIt.exe: C:\Users\ryank\cs128h\cs128honorsfinalproj\LovinIt\src\main.rs
